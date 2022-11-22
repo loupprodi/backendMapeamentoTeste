5cc2cad80f0b498381c39b4716db7bbd400fc3e0
Luiz Felipe V Prosdoskimi <82850974+loupprodi@users.noreply.github.com>
2022-11-22 20:50:11 -0300

    Delete usuario.java
    deletando o arquivo usuario para seguir o padrão Repository

--- mapeamento/ysw/src/main/java/br/com/api/ysw/repository/Usuarios.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.ysw.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api.ysw.model.Usuario;
-
-public interface Usuarios extends JpaRepository<Usuario, Integer> {
-  
-}
