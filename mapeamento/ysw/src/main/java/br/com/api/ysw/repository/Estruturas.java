24d5fa4714cc188832a42b8017b9f37f79521fdf
Luiz Felipe V Prosdoskimi <82850974+loupprodi@users.noreply.github.com>
2022-11-22 20:48:45 -0300

    Delete Estrutura.java
    foi feito o refatoramento do codigo para seguir o padrao repository

--- mapeamento/ysw/src/main/java/br/com/api/ysw/repository/Estruturas.java
+++
@@ -1,8 +0,0 @@
-package br.com.api.ysw.repository;
-
-import br.com.api.ysw.model.Estrutura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Estruturas extends JpaRepository<Estrutura, Integer> {
-  
-}
